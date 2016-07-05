327ee7548a7a47db75f01bc33ac958408dad2e8b
User626 <773059678@qq.com>
2016-07-05 10:13:04 +0800

    布置Main布局

---
+++ app/src/main/java/bean/msg.java
@@ -0,0 +1,7 @@
+package bean;
+
+/**
+ * Created by Administrator on 16-7-4.
+ */
+public class msg{
+}
--- app/src/main/java/com/example/administrator/netease/MainActivity.java
+++ app/src/main/java/com/example/administrator/netease/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.administrator.netease;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import MyFragment.BlankFragment;
 
 public class MainActivity extends AppCompatActivity{
+    String[] urls=new String[]{};
+    private String[] titles;
+    private List<Fragment>fragments;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fragments=new ArrayList<>();
+        initView();
+    }
+
+    private void initView(){
+      titles=new String[]{"头条","娱乐","热点","体育","财经","科技","汽车",
+              "时尚","图片","轻松一刻","段子","军事","历史","家居","独家","游戏","健康",
+              "政务","漫画","哒哒趣闻","彩票","美女"};
+        for(int i = 0; i <titles.length; i++){
+            BlankFragment blankfragment=BlankFragment.newInstance("","");
+        }
     }
 }
