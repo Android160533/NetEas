f1f4dd65584b4fc9c20dc1a3aa017a8bf8ec7894
User626 <773059678@qq.com>
2016-07-05 11:01:56 +0800

    主页面Tablayout与ViewPager关联

--- app/src/main/java/MyFragment/BlankFragment.java
+++ app/src/main/java/MyFragment/BlankFragment.java
@@ -1,6 +1,5 @@
 package MyFragment;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -19,45 +18,27 @@ import com.example.administrator.netease.R;
  * create an instance of this fragment.
  */
 public class BlankFragment extends Fragment{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public BlankFragment(){
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1, String param2){
+    public static BlankFragment newInstance(int index, String param2){
         BlankFragment fragment = new BlankFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt("index", index);
+        args.putString("urls", param2);
         fragment.setArguments(args);
         return fragment;
     }
-
+    int num;
+    String url;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         if(getArguments() != null){
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            num = getArguments().getInt("index");
+            url = getArguments().getString("urls");
         }
     }
 
@@ -74,7 +55,7 @@ public class BlankFragment extends Fragment{
         }
     }
 
-    @Override
+  /*  @Override
     public void onAttach(Context context){
         super.onAttach(context);
         if(context instanceof OnFragmentInteractionListener){
@@ -82,7 +63,7 @@ public class BlankFragment extends Fragment{
         }else{
             throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
-    }
+    }*/
 
     @Override
     public void onDetach(){
--- app/src/main/java/com/example/administrator/netease/MainActivity.java
+++ app/src/main/java/com/example/administrator/netease/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.netease;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -9,19 +11,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import MyFragment.BlankFragment;
+import Myadapter.Fragment_adapter;
 
 public class MainActivity extends AppCompatActivity{
     String[] urls=new String[]{};
     private String[] titles;
     private List<Fragment>fragments;
-
-
+    private  ViewPager vp;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        vp=(ViewPager) findViewById(R.id.vp);
+        tabLayout=(TabLayout) findViewById(R.id.tablayout);
         fragments=new ArrayList<>();
         initView();
     }
@@ -31,7 +36,11 @@ public class MainActivity extends AppCompatActivity{
               "时尚","图片","轻松一刻","段子","军事","历史","家居","独家","游戏","健康",
               "政务","漫画","哒哒趣闻","彩票","美女"};
         for(int i = 0; i <titles.length; i++){
-            BlankFragment blankfragment=BlankFragment.newInstance("","");
+            BlankFragment blankfragment=BlankFragment.newInstance((i+1),"");
+            fragments.add(blankfragment);
         }
+        Fragment_adapter ad=new Fragment_adapter(getSupportFragmentManager(),titles,fragments);
+        vp.setAdapter(ad);
+        tabLayout.setupWithViewPager(vp);
     }
 }
